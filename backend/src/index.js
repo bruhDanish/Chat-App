5c81996aa92cee6f4cccdc0582b657d0977b2892
unknown <syeddh01@gmail.com>
2024-11-26 22:29:39 +0530

    signup and login page done, next profile page

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,8 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 dotenv.config();
 
@@ -11,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes)
