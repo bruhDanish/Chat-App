c23c2c6db9c3d8c29c9663c2cb7bc53b13923ef6
unknown <syeddh01@gmail.com>
2024-12-02 21:57:13 +0530

    implented websocket for real time texting

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,11 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
+import {app, server} from './lib/socket.js';
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 
 dotenv.config();
-
-const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
@@ -21,7 +20,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 })
